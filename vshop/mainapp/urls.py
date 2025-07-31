13e04d1e001603b164e27f53e0cb587ef99f3b20
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-07-31 17:55:41 +0530

    edits

--- vshop/mainapp/urls.py
+++ vshop/mainapp/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('products/add', views.AddProduct.as_view(), name = 'addproduct'),
     path('product/<int:pk>', views.ProductDetails.as_view(), name= 'prod_details'),
     path('product/update/<int:pk>', views.UpdateProduct.as_view(), name = 'updateproduct'),
-    path('product/delete/<int:pk>', views.DeleteProduct.as_view(), name = 'deleteproduct')
+    path('product/delete/<int:pk>', views.DeleteProduct.as_view(), name = 'deleteproduct'),
+    
+
+    path('sendtestemail/', views.send_mail_page)
 ]
--- vshop/vshop/settings.py
+++ vshop/vshop/settings.py
@@ -128,3 +128,14 @@ MEDIA_URL = '/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+from decouple import config, Csv
+
+# Email Configs
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
