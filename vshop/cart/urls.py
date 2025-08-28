3802fd4473b83cf3cfc00e4137b80c372c9e4766
ananthakrishnanpta <ananthakrishnanpta@gmail.com>
2025-08-28 16:55:51 +0530

    added js

--- vshop/cart/urls.py
+++ vshop/cart/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('add/<int:product_id>', views.addToCart, name='add_to_cart'),
     path('remove/<int:cart_item_id>', views.remFromCart, name = 'rem_from_cart'),
             # The following url patterns will be requested by the JS function
-    path('add/<int:cart_item_id>', addQuantity, name='add_quantity'),
-    path('remove/<int:cart_item_id>', remQuantity, name='rem_quantity'),
+    path('add/<int:cart_item_id>', views.addQuantity, name='add_quantity'),
+    path('remove/<int:cart_item_id>', views.remQuantity, name='rem_quantity'),
 
 ]
